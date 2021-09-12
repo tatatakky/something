7562ecc4fc5023049484486287e846d82b50f98a
KodaiTakihara <takihara@rabbix.jp>
2021-09-12 15:05:39 +0900

    add: test for slackGetURLUseCase using scalatest

--- modules/interfaces/src/main/scala/interfaces/repositories/slack/SlackRepositoryImpl.scala
+++ modules/interfaces/src/main/scala/interfaces/repositories/slack/SlackRepositoryImpl.scala
@@ -5,6 +5,10 @@ import entities.domain.slack.{Organization, URL, URLData, URLName}
 import entities.repositories.slack.SlackRepository
 
 class SlackRepositoryImpl extends SlackRepository[IO] {
-  def findURL(urlName: URLName): IO[Option[URLData]] =
-    IO{ Some(URLData( URL("https://www.scala-lang.org/"), Organization("EPFL") )) }
+  def findURL(urlName: URLName): IO[Option[URLData]] = {
+    urlName match {
+      case URLName("scala-lang") => IO{ Some(URLData( URL("https://www.scala-lang.org/"), Organization("EPFL") )) }
+      case _                     => IO{ None }
+    }
+  }
 }
